ab3edbc375ddbd64db26926b8612b20ac82e7cda
rajnish <rajneeshkumar9674737@gmail.com>
2024-09-21 13:04:14 +0530

    update-hero:added wings

--- src/components/Header/Hero.tsx
+++ src/components/Header/Hero.tsx
@@ -36,17 +36,40 @@ const Hero = () => {
                         WebkitMaskPosition: "center",
                     }}
                 ></div>
+                {/* left-wing */}
+                <Image
+                    className="absolute top-1/2 left-1/2 -translate-x-[200%] -translate-y-[200%]  w-1/2 h-1/2 object-contain pointer-events-none -z-10 scale-x-[-1]"
+                    src="/assets/wing.png"
+                    alt="hero"
+                    width={499}
+                    height={100}
+                />
+                {/* right-wing */}
+                <Image
+                    className="absolute top-1/2 left-1/2 translate-x-[80%] -translate-y-[200%]  w-1/2 h-1/2 object-contain pointer-events-none -z-10 "
+                    src="/assets/wing.png"
+                    alt="hero"
+                    width={499}
+                    height={100}
+                />
             </div>
             <div className="relative container top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+            
                 <h1
                     className=" max-md:text-5xl text-8xl font-semibold tracking-tighter  bg-white bg-[radial-gradient(100%_100%_at_top_left,white,rgba(213,129,255,0.5),rgba(74,32,138,0.8))] text-transparent bg-clip-text drop-shadow-[0_4px_6px_rgba(74,32,138,0.8)] -z-10">
                     CODE CRAFT STUDIO
                 </h1>
 
             </div>
-            <div className="absolute left-0 bottom-0 w-1/2 max-md:w-full p-4 text-center right-0 mx-auto">
-                <button className="font-bold my-6 py-2">Studio</button>
-                <p className="text-white/70">
+            <div className="absolute left-0 bottom-0 w-1/2 max-md:w-full p-4 text-center right-0 mx-auto flex items-center flex-col">
+
+                <button className="font-bold border-4 w-16 h-16 rotate-45 border-white my-4 flex items-center justify-center">
+                    <span className="-rotate-45">
+                        Studio
+                    </span>
+                </button>
+
+                <p className="text-white/70 ">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus expedita commodi nam iste molestiae, ad maiores iure nihil explicabo quidem ipsa? Quae quis voluptate dignissimos iure eaque hic adipisci ad.
                 </p>
             </div>
